f24a3140734a94a8095a83750946697d4d5fc10a
Raghav Agarwal <raghav.agarwal2018@vitstudent.ac.in>
2021-08-10 15:59:52 +0530

    add client files

---
+++ client/src/assets/data.js
@@ -0,0 +1,181 @@
+const feed = [
+    {
+        author: {
+            id: "adfasfsadf}",
+            username: "Sparsh",
+        },
+        likes: [
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+        ],
+        comments: [
+            {
+                author: { username: "Sparsh" },
+                id: "fhsajkfhskjf",
+                text:
+                    "Enim duis ad ut duis enim magna ad exercitation nisi fugiat incididunt.",
+            },
+            {
+                author: { username: "Sparsh" },
+                id: "fhsajkfhskjqf",
+                text:
+                    "Consequat consequat mollit duis qui excepteur velit incididunt culpa.",
+            },
+            {
+                id: "fhsajkfhskjf2",
+                author: { username: "Sparsh" },
+                text:
+                    "Qui duis dolor dolor sunt reprehenderit ullamco do pariatur veniam dolor aute nostrud non.",
+            },
+        ],
+        text:
+            "Ipsum ad dolore laboris qui anim irure ut aliquip qui eiusmod. Consequat aute veniam do reprehenderit adipisicing et dolore esse ex elit duis. Aliqua ipsum ipsum nulla nisi amet laboris cillum proident magna. Exercitation non ipsum velit Lorem aliqua. Mollit nostrud aliquip enim duis cupidatat ea aliquip sit aliqua minim aute esse ad. Sunt ut labore velit dolor qui laboris amet nulla et occaecat laborum laboris aliquip do. Sit proident in Lorem pariatur qui velit in veniam est sint.",
+        image:
+            "/data/images/1618086262560_mob_psycho___wallpaper_by_adiim_dd0rfe4~3.jpg",
+        updatedAt: "2021-04-24T18:41:30.916Z",
+    },
+    {
+        author: {
+            id: "adfasfsadf}",
+            username: "Sparsh",
+        },
+        likes: [
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+        ],
+        comments: [
+            {
+                author: { username: "Sparsh" },
+                id: "fhsajkfhskjf",
+                text:
+                    "Enim duis ad ut duis enim magna ad exercitation nisi fugiat incididunt.",
+            },
+            {
+                author: { username: "Sparsh" },
+                id: "fhsajkfhskjqf",
+                text:
+                    "Consequat consequat mollit duis qui excepteur velit incididunt culpa.",
+            },
+            {
+                id: "fhsajkfhskjf2",
+                author: { username: "Sparsh" },
+                text:
+                    "Qui duis dolor dolor sunt reprehenderit ullamco do pariatur veniam dolor aute nostrud non.",
+            },
+        ],
+        text:
+            "Ipsum ad dolore laboris qui anim irure ut aliquip qui eiusmod. Consequat aute veniam do reprehenderit adipisicing et dolore esse ex elit duis. Aliqua ipsum ipsum nulla nisi amet laboris cillum proident magna. Exercitation non ipsum velit Lorem aliqua. Mollit nostrud aliquip enim duis cupidatat ea aliquip sit aliqua minim aute esse ad. Sunt ut labore velit dolor qui laboris amet nulla et occaecat laborum laboris aliquip do. Sit proident in Lorem pariatur qui velit in veniam est sint.",
+        image:
+            "/data/images/1618086272349_wallpaperflare.com_wallpaper (2).jpg",
+        updatedAt: "2021-04-24T18:41:30.916Z",
+    },
+    {
+        author: {
+            id: "adfasfsadf}",
+            username: "Sparsh",
+        },
+        likes: [
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+        ],
+        comments: [
+            {
+                author: { username: "Sparsh" },
+                id: "fhsajkfhskjf",
+                text:
+                    "Enim duis ad ut duis enim magna ad exercitation nisi fugiat incididunt.",
+            },
+            {
+                author: { username: "Sparsh" },
+                id: "fhsajkfhskjqf",
+                text:
+                    "Consequat consequat mollit duis qui excepteur velit incididunt culpa.",
+            },
+            {
+                id: "fhsajkfhskjf2",
+                author: { username: "Sparsh" },
+                text:
+                    "Qui duis dolor dolor sunt reprehenderit ullamco do pariatur veniam dolor aute nostrud non.",
+            },
+        ],
+        text:
+            "Ipsum ad dolore laboris qui anim irure ut aliquip qui eiusmod. Consequat aute veniam do reprehenderit adipisicing et dolore esse ex elit duis. Aliqua ipsum ipsum nulla nisi amet laboris cillum proident magna. Exercitation non ipsum velit Lorem aliqua. Mollit nostrud aliquip enim duis cupidatat ea aliquip sit aliqua minim aute esse ad. Sunt ut labore velit dolor qui laboris amet nulla et occaecat laborum laboris aliquip do. Sit proident in Lorem pariatur qui velit in veniam est sint.",
+        image: "/data/images/1619292594785_hisoka.jpg",
+        updatedAt: "2021-04-24T18:41:30.916Z",
+    },
+    {
+        author: {
+            id: "adfasfsadf}",
+            username: "Sparsh",
+        },
+        likes: [
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+            {
+                id: "fhsajkfhskjf",
+                username: "String",
+            },
+        ],
+        comments: [
+            {
+                author: { username: "Sparsh" },
+                id: "fhsajkfhskjf",
+                text:
+                    "Enim duis ad ut duis enim magna ad exercitation nisi fugiat incididunt.",
+            },
+            {
+                author: { username: "Sparsh" },
+                id: "fhsajkfhskjqf",
+                text:
+                    "Consequat consequat mollit duis qui excepteur velit incididunt culpa.",
+            },
+            {
+                id: "fhsajkfhskjf2",
+                author: { username: "Sparsh" },
+                text:
+                    "Qui duis dolor dolor sunt reprehenderit ullamco do pariatur veniam dolor aute nostrud non.",
+            },
+        ],
+        text:
+            "Ipsum ad dolore laboris qui anim irure ut aliquip qui eiusmod. Consequat aute veniam do reprehenderit adipisicing et dolore esse ex elit duis. Aliqua ipsum ipsum nulla nisi amet laboris cillum proident magna. Exercitation non ipsum velit Lorem aliqua. Mollit nostrud aliquip enim duis cupidatat ea aliquip sit aliqua minim aute esse ad. Sunt ut labore velit dolor qui laboris amet nulla et occaecat laborum laboris aliquip do. Sit proident in Lorem pariatur qui velit in veniam est sint.",
+        image: "/data\\images\\1619293882561_pain.jpeg",
+        updatedAt: "2021-04-24T18:41:30.916Z",
+    },
+];
+module.exports = { feed };
